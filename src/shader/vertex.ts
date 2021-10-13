9bab05f2e757ad955eb542619aaa5ee10793cbae
zhuxudong <callzhuxudong@163.com>
2021-10-13 16:42:17 +0800

    refactor: plane api change

--- src/shader/vertex.ts
+++ src/shader/vertex.ts
@@ -5,7 +5,8 @@ attribute vec2 TEXCOORD_0;
 varying vec2 v_uv;
 
 void main(){
-    gl_Position = vec4( POSITION , 1.0);
+    gl_Position = vec4( POSITION.xzy , 1.0);
+    gl_Position.y *= -1.0;
     v_uv = TEXCOORD_0;
 }
 `;

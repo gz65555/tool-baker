c658b241340cb974c89fec388f79bed35041742b
zhuxudong <callzhuxudong@163.com>
2021-10-28 17:17:34 +0800

    feat: support show background after hdr loader

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import "./loader/HDRLoader";
 export { DecodeMode } from "./enums/DecodeMode";
 export { IBLBaker } from "./IBLBaker";
 export { SphericalHarmonics3Baker } from "./SphericalHarmonics3Baker";
+export { HDRSkyBoxMaterial } from "./HDRSkyBoxMaterial";

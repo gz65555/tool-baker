5913485fa477a60eb57edb9f641a495376627ae2
zhuxudong <callzhuxudong@163.com>
2021-11-12 12:00:03 +0800

    feat: support export buffer and download

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import "./loader/HDRLoader";
 export { DecodeMode } from "./enums/DecodeMode";
 export { IBLBaker } from "./IBLBaker";
 export { SphericalHarmonics3Baker } from "./SphericalHarmonics3Baker";
-export { HDRSkyBoxMaterial } from "./HDRSkyBoxMaterial";
+export { toBuffer } from "./utils/toBuffer";
+export { downloadArrayBuffer } from "./utils/downloadArrayBuffer";

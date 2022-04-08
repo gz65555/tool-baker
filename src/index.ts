b636a1188c2f5f1fed0e5a284aa93fdc879884ed
shensi.zxd <shensi.zxd@alibaba-inc.com>
2022-04-08 17:32:10 +0800

    refactor: move hdr loader to oasis

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import "./loader/HDRLoader";
 export { DecodeMode } from "./enums/DecodeMode";
 export { IBLBaker } from "./IBLBaker";
 export { SphericalHarmonics3Baker } from "./SphericalHarmonics3Baker";

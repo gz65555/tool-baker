51a65b6b0e78ceb21e56c94d2cf63cf14403d8c4
shensi.zxd <shensi.zxd@alibaba-inc.com>
2022-04-21 15:58:03 +0800

    Revert "refactor: move hdr loader to oasis"
    This reverts commit b636a1188c2f5f1fed0e5a284aa93fdc879884ed.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "./loader/HDRLoader";
 export { DecodeMode } from "./enums/DecodeMode";
 export { IBLBaker } from "./IBLBaker";
 export { SphericalHarmonics3Baker } from "./SphericalHarmonics3Baker";
